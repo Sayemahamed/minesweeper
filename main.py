afeaf023e2f77fd02d13382470055d89190fc6a0
Sayem Ahamed <95583680+Sayemahamed@users.noreply.github.com>
2024-06-10 08:21:32 +0600

    added frame

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 from tkinter import *
 
 root = Tk()
+# Override the Settings of the window
+root.config(bg="black")
+root.geometry("1440x720")
+root.title("MineSweeper")
+root.resizable(False, False)
+# Frame
+top_frame= Frame(
+    root, 
+    bg="red",# Change later to black
+    width=1440,
+    height=180
+    )
+top_frame.place(x=0, y=0)
 
+# Run the window
 root.mainloop()
